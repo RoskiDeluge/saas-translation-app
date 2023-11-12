fd5963a0b46025df63afdccb59f6bd060c601daf
RoskiDeluge <roberto.delgado@gmail.com>
2023-11-11 23:48:26 -1000

    first commit

---
+++ components/Logo.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+
+export default function Logo() {
+  return (
+    <Link href="/" prefetch={false} className="overflow-hidden">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center"
+        >
+          <div className="dark:filter dark:invert">OmniChat</div>
+        </AspectRatio>
+      </div>
+    </Link>
+  );
+}
